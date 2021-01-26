14e00749190ab78237f6100f05d93129d65756e2
hiryamada <71864722+hiryamada@users.noreply.github.com>
2021-01-26 11:39:15 +0900

    update

---
+++ AZ-204/sample/blob/Program.cs
@@ -0,0 +1,117 @@
+﻿/*
+
+document: https://docs.microsoft.com/ja-jp/azure/storage/blobs/storage-quickstart-blobs-dotnet
+
+dotnet new console -n blobtest
+cd blobtest
+dotnet add package Azure.Storage.Blob
+
+*/
+
+using System;
+using Azure.Storage.Blobs;
+
+// 接続文字列
+var connectionString = "DONT-WRITE-HERE";
+
+void Print(object value)
+{
+    Console.WriteLine(value);
+}
+
+string Read(string prompt)
+{
+    Print(prompt);
+    return Console.ReadLine();
+}
+
+void SetConnectionString()
+{
+    connectionString = Read("Connection String:");
+}
+
+void ListContainers()
+{
+    var bsc = new BlobServiceClient(connectionString);
+    foreach (var container in bsc.GetBlobContainers())
+    {
+        Console.WriteLine(container.Name);
+    }
+}
+
+void CreateContainer() 
+{
+    Console.WriteLine("Container name:");
+    var containerName = Console.ReadLine();
+    var bcc = new BlobContainerClient(connectionString, containerName);
+    bcc.CreateIfNotExists();
+}
+
+void DeleteContainer() 
+{
+    Console.WriteLine("Container name:");
+    var containerName = Console.ReadLine();
+    var bcc = new BlobContainerClient(connectionString, containerName);
+    bcc.Delete();
+}
+
+void Upload()
+{
+    string containerName = Read("Container name:");
+    string localFileName = Read("Local file name:");
+    var bc = new BlobClient(connectionString, containerName, localFileName);
+    bc.Upload(localFileName);
+}
+
+void Download()
+{
+    string containerName = Read("Container name:");
+    string blobName = Read("Blob name:");
+    var bc = new BlobClient(connectionString, containerName, blobName);
+    bc.DownloadTo(blobName);
+}
+
+void ListBlobs()
+{
+    string containerName = Read("Container name:");
+    var bcc = new BlobContainerClient(connectionString, containerName);
+    foreach (var blob in bcc.GetBlobs())
+    {
+        Print(blob.Name);
+    }
+}
+
+SetConnectionString();
+
+while (true)
+{
+    Print("Command:");
+    var input = Console.ReadLine();
+    switch (input) {
+        case "ListContainers":
+            ListContainers();
+            break;
+        case "CreateContainer":
+            CreateContainer();
+            break;
+        case "DeleteContainer":
+            DeleteContainer();
+            break;
+        case "ListBlobs":
+            ListBlobs();
+            break;
+        case "Upload":
+            Upload();
+            break;
+        case "Download":
+            Download();
+            break;
+        case "Exit":
+        case "Quit":
+            return;
+        default:
+            Print("Unknown command: " + input);
+            break;
+    }
+}
+
