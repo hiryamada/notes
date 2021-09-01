4791a45507868d846551a438ca3e5686b0bdfde1
hiryamada <71864722+hiryamada@users.noreply.github.com>
2021-09-01 11:41:05 +0900

    update

--- AZ-204/sample/cosmosdb/Program.cs
+++ AZ-204/sample/cosmosdb/Program.cs
@@ -14,97 +14,109 @@ dotnet run
 using System;
 using Microsoft.Azure.Cosmos;
 
-var connectionString = "DONT-WRITE-HERE";
-
-void Print(object value) 
+namespace test
 {
-    Console.WriteLine(value);
-}
-
-string Read(string prompt)
-{
-    Print(prompt);
-    return Console.ReadLine();
-}
-
-void SetConnectionString() 
-{
-    connectionString = Read("Connection string:");
-}
-
-void CreateDatabase()
-{
-    var databaseId = Read("Database id:");
-    var cc = new CosmosClient(connectionString);
-    cc.CreateDatabaseAsync(databaseId).Wait();
-}
-
-void CreateContainer()
-{
-    var databaseId = Read("Database id:");
-    var containerName = Read("Container name:");
-    var partitionKeyPath = Read("Partition key path:");
-    var cc = new CosmosClient(connectionString);
-    var db = cc.GetDatabase(databaseId);
-    db.CreateContainerAsync(containerName, partitionKeyPath);
-}
-
-void CreateItem()
-{
-    var databaseId = Read("Database id:");
-    var containerName = Read("Container name:");
-    var songTitle = Read("songTitle(id):");
-    var artist = Read("artist:");
-    var music = new Music {Id = songTitle, Artist = artist};
-
-    var cc = new CosmosClient(connectionString);
-    var db = cc.GetDatabase(databaseId);
-    var container = db.GetContainer(containerName);    
-    container.CreateItemAsync(music);
-}
-
-void ListItems()
-{
-    var databaseId = Read("Database id:");
-    var containerName = Read("Container name:");
-    var cc = new CosmosClient(connectionString);
-    var db = cc.GetDatabase(databaseId);
-    var container = db.GetContainer(containerName);    
-    var query = "select * from m";
-    using var fe = container.GetItemQueryIterator<Music>(query);
-    while (fe.HasMoreResults)
+    class Program
     {
-        foreach (var music in fe.ReadNextAsync().Result)
+
+        static string connectionString = "DONT-WRITE-HERE";
+
+        static void Print(object value) 
         {
-            Print("id: " + music.Id + ", artist: " + music.Artist);
+            Console.WriteLine(value);
         }
-    }
-}
 
-SetConnectionString();
+        static string Read(string prompt)
+        {
+            Print(prompt);
+            return Console.ReadLine();
+        }
 
-while (true)
-{
-    var command = Read("Command:");
-    switch (command)
-    {
-        case "CreateDatabase":
-            CreateDatabase();
-            break;
-        case "CreateContainer":
-            CreateContainer();
-            break;
-        case "CreateItem":
-            CreateItem();
-            break;
-        case "ListItems":
-            ListItems();
-            break;
-        case "Exit":
-        case "Quit":
-            return;
-        default:
-            Print("Unknown command: " + command);
-            break;
+        static void SetConnectionString() 
+        {
+            connectionString = Read("Connection string:");
+        }
+
+        static void CreateDatabase()
+        {
+            var databaseId = Read("Database id:");
+            var cc = new CosmosClient(connectionString);
+            cc.CreateDatabaseAsync(databaseId).Wait();
+        }
+
+        static void CreateContainer()
+        {
+            var databaseId = Read("Database id:");
+            var containerName = Read("Container name:");
+            var partitionKeyPath = Read("Partition key path:");
+            var cc = new CosmosClient(connectionString);
+            var db = cc.GetDatabase(databaseId);
+            db.CreateContainerAsync(containerName, partitionKeyPath);
+        }
+
+        static void CreateItem()
+        {
+            var databaseId = Read("Database id:");
+            var containerName = Read("Container name:");
+            var songTitle = Read("songTitle(id):");
+            var artist = Read("artist:");
+            var music = new Music {Id = songTitle, Artist = artist};
+
+            var cc = new CosmosClient(connectionString);
+            var db = cc.GetDatabase(databaseId);
+            var container = db.GetContainer(containerName);    
+            container.CreateItemAsync(music);
+        }
+
+        static void ListItems()
+        {
+            var databaseId = Read("Database id:");
+            var containerName = Read("Container name:");
+            var cc = new CosmosClient(connectionString);
+            var db = cc.GetDatabase(databaseId);
+            var container = db.GetContainer(containerName);    
+            var query = "select * from m";
+            using var fe = container.GetItemQueryIterator<Music>(query);
+            while (fe.HasMoreResults)
+            {
+                foreach (var music in fe.ReadNextAsync().Result)
+                {
+                    Print("id: " + music.Id + ", artist: " + music.Artist);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+        SetConnectionString();
+
+            while (true)
+            {
+                var command = Read("Command:");
+                switch (command)
+                {
+                    case "CreateDatabase":
+                        CreateDatabase();
+                        break;
+                    case "CreateContainer":
+                        CreateContainer();
+                        break;
+                    case "CreateItem":
+                        CreateItem();
+                        break;
+                    case "ListItems":
+                        ListItems();
+                        break;
+                    case "Exit":
+                    case "Quit":
+                        return;
+                    default:
+                        Print("Unknown command: " + command);
+                        break;
+                }
+            }
+
+        }
     }
 }
